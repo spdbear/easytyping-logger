026a682e89f854ced0b94414d551cb46214d2080
spdbear <16793732+spdbear@users.noreply.github.com>
2020-02-27 18:51:15 +0900

    OCR実装

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 import pyocr
 import pyocr.builders
 
-import cv2
 from PIL import Image
+import os
+import datetime
 
 
 class TypingResult:
@@ -38,14 +39,37 @@ def imageToText(src):
     tool = tools[0]
 
     dst = tool.image_to_string(
-        Image.open(src),
+        src,
         builder=pyocr.builders.DigitBuilder(tesseract_layout=6)
     )
     return dst
 
 
 if __name__ == "__main__":
-    img_path = "./img/img99091090014.jpg"
-    out = imageToText(img_path)
-    img = cv2.imread(img_path)
-    print(out)
+    img_path = "./img/img99512066032.jpg"
+
+    img = Image.open(img_path).convert("L")
+    img.point(lambda x: 0 if x < 128 else x)
+    crops = {"types": (281, 145, 330, 172),
+             "time_m": (251, 196, 273, 221),
+             "time_s": (296, 196, 332, 221),
+             "speed": (271, 245, 333, 273),
+             "misstypes": (297, 295, 333, 332),
+             }
+
+    ocr_result = {}
+    output = {}
+
+    for k, v in crops.items():
+        out = imageToText(img.crop(v))
+        ocr_result[k] = out
+
+    output["time"] = int(ocr_result["time_m"]) * 60 + int(ocr_result["time_s"])
+    output["missrate"] = int(ocr_result["misstypes"]) / \
+        int(ocr_result["types"])
+    output["date"] = datetime.datetime.fromtimestamp(
+        os.stat(img_path).st_atime).strftime("%Y/%m/%d %H:%M:%S")
+    output["misstypes"] = ocr_result["misstypes"]
+    output["types"] = ocr_result["types"]
+    output["speed"] = ocr_result["speed"]
+    print(output)
