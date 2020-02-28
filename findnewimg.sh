b294db80852980ea342f4f8b4795959a34a623c3
spdbear <16793732+spdbear@users.noreply.github.com>
2020-02-28 19:53:56 +0900

    構成を変更

--- findnewimg.sh
+++
@@ -1,3 +0,0 @@
- #!/bin/zsh
- imgpath=$(ls -1t ~/Downloads/img*.jpg | head -1)
- python main.py ${imgpath}
--- getenv.py
+++ getenv.py
@@ -9,3 +9,4 @@ load_dotenv(dotenv_path)
 
 CREDENTIAL_JSON_PATH = os.environ.get("CREDENTIAL_JSON_PATH")
 SPREADSHEET_KEY = os.environ.get("SPREADSHEET_KEY")
+TARGET_DIR = os.environ.get("TARGET_DIR")
