4cb79d33059b3dca47c3d1040edc4a9bc576efbd
spdbear <16793732+spdbear@users.noreply.github.com>
2020-02-28 23:40:22 +0900

    delete test

--- test.py
+++
@@ -1,6 +0,0 @@
-import main
-
-print(main.ocr_from_image("/Users/yan/Downloads/img99688663778.jpg"))
-print(main.ocr_from_image("img/img99091090014.jpg"))
-print(main.ocr_from_image("img/img99091272384.jpg"))
-print(main.ocr_from_image("img/img99511903953.jpg"))
